97cd458f17628d2dd888774efd1309999c2420dd
masaru-iwase <masaru5floor@gmail.com>
2020-12-01 22:16:13 +0900

    新規登録ユーザー画面遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    if user.new(user_params)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def user_params
+    params.rquire(:user).permit(:name, :email)
+  end
 end

83458fc9fd18f7beb13c81d9467053cb3a8105ed
masaru-iwase <masaru5floor@gmail.com>
2020-12-02 23:12:44 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, 
+  validates :name, uniqueness: true
+  validates :nick_name, presence: true,
+  validates :mail, presence: true,
+  validates :mail, uniqueness: true.
+  validates :mail, inclusion: {in:%w(@)},
+  validates :password, length: {minimum: 6},
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: {with: VALID_PASSWROD_REGEX }
+  validates :password, password_confirmation, confirmation: true
 end
